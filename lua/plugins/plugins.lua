f9899e73a34cc2fe62956ef3508849df646d5cf1
Saad Dev <mahmoodsaad540@gmail.com>
2025-05-25 20:54:00 +0530

    Added All the plugins to 'lua/plugins/plugins.lua'

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,3 +1,16 @@
 return {
+  -- For themes
   { "projekt0n/github-nvim-theme", name = "github-theme" },
+
+  -- For Plugins
+  { "bash-lsp/bash-language-server", build = "npm install -g bash-language-server" },
+  { "psf/black" },
+  { "microsoft/pyright" },
+  { "charliermarsh/ruff" },
+  { "python/mypy" },
+  { "microsoft/debugpy" },
+  { "neovim/nvim-lspconfig" },
+  { "LuaLS/lua-language-server" },
+  { "JohnnyMorganz/stylua" },
+  { "koalaman/shellcheck" },
 }
